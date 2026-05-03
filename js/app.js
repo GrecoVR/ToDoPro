a3b5819cb4f632048b583ee8d135b55ab16a20c9
Greco Vallejos <grecovallejos07081976@gmail.com>
2026-05-03 16:34:17 -0400

    feat: Completar tareas y añadir filtros

--- js/app.js
+++ js/app.js
@@ -1,41 +1,81 @@
 let tareasguardadas = JSON.parse(localStorage.getItem('tareas')) || [];
+let filtro = 'todasLasTareas';
 const formularioTarea = document.getElementById('formulario-tarea');
 const nombreTarea = document.getElementById('nombre-tarea');
 const listaTareas = document.getElementById('lista-tareas');
+const filtroTareas = document.querySelectorAll('.nav-list a');
 render();
 
+filtroTareas.forEach(boton => {
+    boton.addEventListener('click', (e) => {
+        e.preventDefault();
+
+        const tarea = boton.textContent;
+        if(tarea.includes('Todas las tareas')) {
+            filtro = 'todasLasTareas';
+        } else if(tarea.includes('Tareas completadas')) {
+            filtro = 'tareasCompletadas';
+        } else if(tarea.includes('Tareas pendientes')) {
+            filtro = 'tareasPendientes';
+        }
+        render();
+    });
+});
 formularioTarea.addEventListener('submit', function(event) {
     event.preventDefault();
     const tarea = nombreTarea.value.trim();
     if (tarea){
-        agregarTarea(tarea);
-        tareasguardadas.push(tarea);
-        localStorage.setItem('tareas', JSON.stringify(tareasguardadas));
+        tareasguardadas.push({
+            texto: tarea,
+            completed: false
+        });
+        guardarTareas();
         render();
     }
     nombreTarea.value = '';
 });
 
-function agregarTarea(tarea) {
-    const li = document.createElement('li');
-    li.textContent = tarea;
-    listaTareas.appendChild(li);
-}
-
 function eliminarTarea(index) {
     tareasguardadas.splice(index, 1);
-    localStorage.setItem('tareas', JSON.stringify(tareasguardadas));
+    guardarTareas();
     render();
 }
 function render(){
     listaTareas.innerHTML = '';
-    tareasguardadas.forEach((tarea, index) => {
+    let tareasFiltradas;
+    if (filtro === 'tareasCompletadas') {
+        tareasFiltradas = tareasguardadas.filter(tarea => tarea.completed);
+    } else if (filtro === 'tareasPendientes') {
+        tareasFiltradas = tareasguardadas.filter(tarea => !tarea.completed);
+    } else {
+        tareasFiltradas = tareasguardadas;
+    }
+    tareasFiltradas.forEach((tarea, index) => {
         const li = document.createElement('li');
-        li.textContent = tarea;
+        li.textContent = tarea.texto;
+        if (tarea.completed){
+            li.classList.add('completed');
+        }
+
+        li.addEventListener('click', () => {
+            tarea.completed = !tarea.completed;
+            guardarTareas();
+            render();
+        });
+
         const botonEliminar = document.createElement('button');
         botonEliminar.textContent = 'Eliminar';
-        botonEliminar.addEventListener('click', () => eliminarTarea(index));
+        botonEliminar.addEventListener('click', (e) => {
+            e.stopPropagation();
+            eliminarTarea(index);
+        });
+           
         li.appendChild(botonEliminar);
         listaTareas.appendChild(li);
     });
 }
+
+function guardarTareas() {
+    localStorage.setItem('tareas', JSON.stringify(tareasguardadas));
+}
+
